c4ac638f5e22c95df1966b29be603346e8414072
Mountain-PNS <sonpn30176@gmail.com>
2024-04-13 14:47:08 +0700

    update

--- src/page/auth/Login.tsx
+++ src/page/auth/Login.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../../assets/img/disney.webp";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  return <>Login</>;
+};
 
-export default Login
+export default Login;
